1218a0fb16d9fc84135ac9c59e68cfa9464fb2ad
Jorge Dobkovski <47338154+jorgedobkovski@users.noreply.github.com>
2023-11-13 15:28:31 -0400

    creating a simple web scraping with c#

---
+++ Driver/WebScraper.cs
@@ -0,0 +1,39 @@
+﻿using EasyAutomationFramework;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebScraping.Model;
+
+namespace WebScraping.Driver
+{
+    public class WebScraper : Web
+    {
+        public DataTable GetData(string url)
+        {
+            if (driver == null) StartBrowser();
+
+            var items = new List<Item>();
+
+            Navigate(url);
+
+            var elements = GetValue(TypeElement.Xpath, "/html/body/div[1]/div[3]/div/div[2]/div")
+                            .element.FindElements(By.ClassName("thumbnail"));
+
+            foreach (var element in elements)
+            {
+                var item = new Item();
+                item.Title = element.FindElement(By.ClassName("title")).GetAttribute("title");
+                item.Description = element.FindElement(By.ClassName("description")).Text;
+                item.Price = element.FindElement(By.ClassName("price")).Text;
+
+                items.Add(item);
+            }
+
+            return Base.ConvertTo(items);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EasyAutomationFramework;
+using EasyAutomationFramework.Model;
+using WebScraping.Driver;
+
+var web = new WebScraper();
+
+var computers = web.GetData("https://webscraper.io/test-sites/e-commerce/allinone/computers");
+var laptops = web.GetData("https://webscraper.io/test-sites/e-commerce/allinone/computers/laptops");
+var tablets = web.GetData("https://webscraper.io/test-sites/e-commerce/allinone/computers/tablets");
+
+var paramss = new ParamsDataTable("Dados", @"C:\temp\excel", new List<DataTables>()
+{
+    new DataTables("computers", computers),
+    new DataTables("laptops", laptops),
+    new DataTables("tablets", tablets),
+});
+
+Base.GenerateExcel(paramss);
